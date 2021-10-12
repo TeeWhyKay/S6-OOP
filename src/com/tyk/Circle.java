128a40cf76039fc257e220b007bf9db6f5f3a550
YongKeeTan <yongkee.t@gmail.com>
2021-10-12 17:56:12 +0800

    implemented parent circle and child cylinder class

---
+++ src/com/tyk/Circle.java
@@ -0,0 +1,18 @@
+package com.tyk;
+
+public class Circle {
+    private double radius;
+
+    public Circle(double radius) {
+        if (radius < 0) radius = 0;
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double getArea() {
+        return Math.PI * Math.pow(radius, 2);
+    }
+}
