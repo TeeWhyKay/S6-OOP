191ab669c9802a76a23e1ea277b5904aab741b2c
YongKeeTan <yongkee.t@gmail.com>
2021-10-12 18:15:30 +0800

    refactored code to rename wrong parameter in rectangle constructor

--- src/com/tyk/Rectangle.java
+++ src/com/tyk/Rectangle.java
@@ -4,7 +4,7 @@ public class Rectangle {
     private double width;
     private double length;
 
-    public Rectangle(double width, double height) {
+    public Rectangle(double width, double length) {
         if (width < 0) width = 0;
         if (length < 0) length = 0;
         this.width = width;
